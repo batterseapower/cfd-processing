936d0c60cd8171a7a5e33e61bb2b20df1338f28b
Max Bolingbroke <batterseapower@hotmail.com>
2010-03-07 01:09:31 +0000

    Various tweaks to the munger

--- munge.py
+++ munge.py
@@ -2,6 +2,7 @@
 
 import csv
 import sys
+import os.path
 import re
 from math import sqrt, log
 
@@ -21,7 +22,10 @@ def unions(sets):
 
 def printconstants(title, ks):
     print title
-    for name, value in ks.items():
+    for name, value in ks:
+        if type(value) == type(0.0):
+            value = "%.6g" % value
+        
         print "  ", name, "=", value
 
 
@@ -46,29 +50,34 @@ def uplus_theory(yplus):
 
 
 # Print some information about the model
-printconstants("Model constants:", { "width" : width, "height" : height, "d_h" : d_h, "density" : density, "mu" : mu })
+printconstants("Model constants:", [("width", width), ("height", height), ("d_h", d_h), ("density", density), ("mu", mu)])
 
 
 # Pull in data
-pl_wall_functions = {}
-pl_scaled_velocity_profiles = {}
+sim_wall_functions = {}
+sim_scaled_velocity_profiles = {}
 for file in sys.argv[1:]:
     print ""
     print "#", file
     
     # pl29sf1.13base0.1distance.csv
     # r"pl(\d+)sf([\d\.]+)base([\d\.]+)distance"
+    file_base = os.path.splitext(file)[0]
     
     # Parse our prism layer count
-    m = re.search(r"pl(\d+)", file)
+    m = re.search(r"pl(\d+)", file_base)
     if m is None:
         print "Could not determine prism layer count: add plN to the file name"
         sys.exit(1)
     else:
         pl = int(m.group(1))
     
+    # Parse out stretch factor
+    m = re.search(r"sf([\d\.]+)", file_base)
+    sf = m is None and "unknown" or m.group(1)
+    
     # Parse out wall shear stress
-    m = re.search(r"tw([\d\.]+)", file)
+    m = re.search(r"tw([\d\.]+)", file_base)
     if m is None:
         print "Count not determine wall shear stress: add twN.M to the file name"
         sys.exit(1)
@@ -87,14 +96,26 @@ for file in sys.argv[1:]:
         return velocity / ustar
 
     # Print some information about the simulation
-    printconstants("Simulation constants:", { "pl" : pl, "wall shear stress" : wall_shear_stress, "ustar" : ustar, "d_v" : d_v })
+    printconstants("Simulation constants:", [("pl", pl), ("wall shear stress", wall_shear_stress), ("ustar", ustar), ("d_v", d_v)])
     
     # "Position [0.0, 1.0, 0.0] (m)-point 1 (m)","Velocity: Magnitude-point 1 (m/s)"
     # 0.0,35.15489668712291
     # 2.499999993688107E-7,35.15489668712291
     reject_count = 0
     raw_velocity_positions = []
-    for row in drop(1, csv.reader(open(file))):
+    
+    # Skip header row
+    reader = csv.reader(open(file))
+    header = reader.next()
+    is_wall_distance = "Wall Distance" in header[0]
+
+    if is_wall_distance:
+        print "WARNING: using wall-distance position information, results may be inaccurate"
+    
+    # Input body of data
+    all_rows = list(reader)
+    last_position = None
+    for row in all_rows:
         # Verify record is correct length
         if len(row) != 2:
             reject_count = reject_count + 1
@@ -102,7 +123,28 @@ for file in sys.argv[1:]:
         
         # Verify we can parse as numbers
         try:
-            raw_velocity_positions.append((float(row[0]), float(row[1])))
+            position, velocity = float(row[0]), float(row[1])
+            
+            # If reading a file containing wall distance, we only want half of the profile
+            # or we get weirdy discontinituites in the output
+            if is_wall_distance and position < last_position:
+                # Diagnostics for the user to help them figure out disontinutities
+                broke_at = len(raw_velocity_positions)
+                expected_break_at = len(all_rows) / 2
+                row_different = expected_break_at - broke_at
+                
+                print "Breaking wall-distance series at", position, "<", last_position
+                print "Broke after", broke_at, "rows, expecting", expected_break_at, "rows"
+                if row_different > 1:
+                    print "CRITICAL WARNING: the number of rows got is less than our expectation by", row_different, "so the series is probably truncated"
+                elif row_different < -1:
+                    print "CRITICAL WARNING: the number of rows got exceeds our expectation by", abs(row_different), "so the series may contain false readings"
+                
+                break
+            else:
+                last_position = position
+            
+            raw_velocity_positions.append((position, velocity))
         except ValueError:
             reject_count = reject_count + 1
     
@@ -116,7 +158,7 @@ for file in sys.argv[1:]:
         # Record the minimum position that experienced a given velocity
         velocity_positions[velocity] = min(velocity_positions.get(velocity, sys.maxint), position)
     
-    velocity_profile = sorted([(position, velocity) for velocity, position in list(velocity_positions.items())])
+    velocity_profile = sorted([(position, velocity) for velocity, position in velocity_positions.items()])
     
     # Determine average velocity
     distance_accumulator = 0.0
@@ -132,17 +174,50 @@ for file in sys.argv[1:]:
     velocity_average = velocity_average_accumulator / distance_accumulator
 
     # Intermediate results
-    printconstants("Simulation summary:", { "average velocity" : velocity_average, "maximum velocity" : max([velocity for position, velocity in raw_velocity_positions]) })
-
+    positions, velocities = zip(*raw_velocity_positions)
+    printconstants("Simulation summary:", [("average velocity", velocity_average),
+                                           ("minimum velocity", min(velocities)),
+                                           ("maximum velocity", max(velocities)),
+                                           ("minimum position", min(positions)),
+                                           ("maximum position", max(positions)),
+                                           ("minimum non-zero y+", yplus(min([position for position in positions if position > 0.0])))])
+    
+    # Trim data down to size (especially important if the velocity profile was built from position-indexed information)
+    half_velocity_profile = [(position, velocity) for position, velocity in velocity_profile if position <= (wall_to_wall_distance / 2) and position > 0.0]
+    
     # Build data for graphing
-    half_velocity_profile = [(position, velocity) for position, velocity in velocity_profile if position <= (wall_to_wall_distance / 2)]
-    pl_wall_functions[pl] = [(yplus(position), uplus(velocity)) for position, velocity in half_velocity_profile]
-    pl_scaled_velocity_profiles[pl] = [(velocity / velocity_average, 2 * position / d_h) for position, velocity in half_velocity_profile]
+    sim = "pl=" + str(pl) + ",sf=" + sf
+    sim_wall_functions[sim] = [(yplus(position), uplus(velocity)) for position, velocity in half_velocity_profile]
+    sim_scaled_velocity_profiles[sim] = [(velocity / velocity_average, 2 * position / d_h) for position, velocity in half_velocity_profile]
 
 
 # Find the theoretical wall function
-all_yplus = unions([set([yplus for yplus, _ in wall_function]) for wall_function in pl_wall_functions.values()])
-theory_wall_function = [(yplus, uplus_theory(yplus)) for yplus in all_yplus]
+all_yplus = unions([set([yplus for yplus, _ in wall_function]) for wall_function in sim_wall_functions.values()])
+theory_wall_function = sorted([(yplus, uplus_theory(yplus)) for yplus in all_yplus])
+
+
+# Compute best fit to the theory
+# import bisect
+# 
+# print ""
+# print "# Distance from theory:"
+# best_match, best_match_distance = None, sys.maxint
+# for sim, wall_function in sim_wall_functions.items():
+#     wall_function_ypluses = [yplus for yplus, _ in wall_function]
+#     
+#     def wall_function_at(yplus):
+#         # Linearly interpolate to approximate the wall function at this point
+#         i = bisect.bisect(wall_function_ypluses, yplus)
+#         yplus1, uplus1 = wall_function[i]
+#         yplus2, uplus2 = wall_function[i - 1]
+#         return uplus1 + (yplus - yplus1) * (uplus2 - uplus1) / (yplus2 - yplus1)
+#     
+#     sum_sqs_distance = sum([pow(wall_function_at(yplus) - uplus_theory, 2) for yplus, uplus_theory in theory_wall_function])
+#     print "  ", sim + ":", sum_sqs_distance
+#     if sum_sqs_distance < best_match_distance:
+#         best_match, best_match_distance = sim, sum_sqs_distance
+# 
+# print "# Best match:", best_match
 
 
 # OK, lets go:
@@ -151,26 +226,29 @@ import matplotlib.pyplot as plt
 
 # y+ vs u+:
 plt.clf()
+plt.figure(figsize=(10, 8), dpi=80)
 
 plt.xlabel("y+")
 plt.ylabel("u+")
 
-for label, wall_function in [("theory", theory_wall_function)] + [("pl=" + str(pl), wall_function) for pl, wall_function in pl_wall_functions.items()]:
+for sim, wall_function in [("theory", theory_wall_function)] + sim_wall_functions.items():
     yplus, uplus = zip(*wall_function)
-    plt.semilogx(yplus, uplus, label=label)
+    plt.semilogx(yplus, uplus, label=sim)
 
+#plt.axis([0.1, 110, 0, 25])
 plt.legend(loc="upper left")
 plt.savefig("yplus-vs-uplus")
 
 # scaled velocity:
 plt.clf()
+plt.figure(figsize=(10, 8), dpi=80)
 
 plt.xlabel("u/u_avg")
 plt.ylabel("2y/D_H")
 
-for pl, scaled_velocity_profile in pl_scaled_velocity_profiles.items():
+for sim, scaled_velocity_profile in sim_scaled_velocity_profiles.items():
     scaled_velocity, scaled_y = zip(*scaled_velocity_profile)
-    plt.plot(scaled_velocity, scaled_y, label="pl=" + str(pl))
+    plt.plot(scaled_velocity, scaled_y, label=sim)
 
 plt.legend(loc="upper left")
 plt.savefig("scaled-velocity-profile")
